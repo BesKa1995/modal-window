7a97f2cbe29780e027ddf2ff3af8066e9677b0b4
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-07 02:25:50 +0400

    add modal open close functionality

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,5 +1,45 @@
-// $.modal = function() {
+function _creatModal(options) {
+  const modal = document.createElement('div')
+  modal.classList.add('bmodal')
+  modal.insertAdjacentHTML('afterbegin', `
+      <div class="bmodal">
+        <div class="modal-overlay">
+          <div class="modal-window">
+            <div class="modal-header">
+              <span class="modal-title">Modal title</span>
+              <span class="modal-close">x</span>
+            </div>
+            <div class="modal-body">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="modal-footer">
+              <button>Ok</button>
+              <button>Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  `)
+  document.body.appendChild(modal)
+  return modal
+}
 
-// }
 
-console.log($)
+
+$.modal = function(options) {
+  const $modal = _creatModal(options)
+  return {
+    open() {
+      $modal.classList.add('open')
+    },
+    close() {
+      $modal.classList.remove('open')
+    },
+    destroy() {
+
+    }
+  }
+}
+
+
