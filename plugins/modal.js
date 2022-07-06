7903ca786c8c2d28aae762b932198e0611bab681
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-07 03:00:16 +0400

    add modal animation

--- plugins/modal.js
+++ plugins/modal.js
@@ -8,17 +8,16 @@ function _creatModal(options) {
             <div class="modal-header">
               <span class="modal-title">Modal title</span>
               <span class="modal-close">x</span>
-            </div>
-            <div class="modal-body">
-              <p>Lorem ipsum dolor sit amet.</p>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
+            </div <div class="modal-body">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
             <div class="modal-footer">
               <button>Ok</button>
               <button>Cancel</button>
             </div>
           </div>
         </div>
+      </div>
       </div>
   `)
   document.body.appendChild(modal)
@@ -28,13 +27,27 @@ function _creatModal(options) {
 
 
 $.modal = function(options) {
+
+  const ANIMATION_SPEED = 2000 //ms
   const $modal = _creatModal(options)
+
+  let closing = false
   return {
     open() {
+      if (closing) return // early exti
+
       $modal.classList.add('open')
     },
     close() {
+      closing = true
+
       $modal.classList.remove('open')
+      $modal.classList.add('hide')
+
+      setTimeout(() => {
+        closing = false
+        $modal.classList.remove('hide')
+      }, ANIMATION_SPEED)
     },
     destroy() {
 
