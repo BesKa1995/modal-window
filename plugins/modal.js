6f78238c6aea9380e82d085815d4f397f1226b80
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-07 15:31:27 +0400

    add removeEventListener to modal

--- plugins/modal.js
+++ plugins/modal.js
@@ -60,6 +60,7 @@ $.modal = function(options) {
   return Object.assign(modal, {
     destroy() {
       $modal.parentNode.removeChild($modal)
+      $modal.removeEventListener(listener)
       destroyed = true
     }
   })
